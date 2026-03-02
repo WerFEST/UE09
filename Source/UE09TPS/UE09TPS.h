7b09c9b4b79b0bcc7a9fdc1a3721dd90165a826e
WerFEST <werfest2@gmail.com>
2026-03-02 23:28:10 +0500

    Added USTRUCT for CharStats

--- Source/UE09TPS/UE09TPS.h
+++ Source/UE09TPS/UE09TPS.h
@@ -4,5 +4,35 @@
 
 #include "CoreMinimal.h"
 
+#include "UE09TPS.generated.h"
+
+
 /** Main log category used across the project */
 DECLARE_LOG_CATEGORY_EXTERN(LogUE09TPS, Log, All);
+
+
+USTRUCT(BlueprintType)
+struct FTestStats
+{
+	GENERATED_BODY()
+
+public:
+
+	FTestStats() : Health(100.0f), Stamina(100.0f), Level(1)
+	{
+	}
+
+	UPROPERTY()
+	float Health;
+
+	UPROPERTY()
+	float Stamina;
+
+	UPROPERTY()
+	int32 Level;
+
+
+
+
+
+};
